b6e05c796cb00128c90790c48ac6f18cbc6baa3d
Tiphaine LAURENT <tiphaine.laurent@epitech.eu>
2018-12-07 11:10:54 +0100

    add camera rotation

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -4,37 +4,48 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    private Vector3 firstPoint;
-    private Vector3 secondPoint;
-    private float xAngle = 0;
-    private float yAngle = 0;
-    private float xAngleTmp = 0;
-    private float yAngleTmp = 0;
+    private Vector2 from;
+    private float turnSpeed = 5f;
+    private Vector2 movement;
 
     // Use this for initialization
     void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.touchCount > 0)
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount == 2)
         {
-            if (Input.GetTouch(0).phase == TouchPhase.Began)
-            {
-                firstPoint = Input.GetTouch(0).position;
-                xAngleTmp = xAngle;
-                yAngleTmp = yAngle;
-            }
+            Touch touch1 = Input.GetTouch(0);
+            Touch touch2 = Input.GetTouch(1);
 
-            if (Input.GetTouch(0).phase == TouchPhase.Moved)
+            switch (touch2.phase)
             {
-                secondPoint = Input.GetTouch(0).position;
-                xAngle = xAngleTmp + (secondPoint.x - firstPoint.x) * 180 / Screen.width;
-                yAngle = yAngleTmp + (secondPoint.y - firstPoint.y) * 90 / Screen.height;
+                case TouchPhase.Began:
+                    {
+                        from = touch1.position - touch2.position;
+                        break;
+                    }
+                case TouchPhase.Moved:
+                    {
+                        Vector2 current = touch1.position - touch2.position;
+                        float targetAngle = Vector2.Angle(current, from);
 
-                this.transform.rotation = Quaternion.Euler(yAngle, xAngle, 0);
+                        if (targetAngle > 10)
+                        {
+                            this.transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 0, targetAngle), turnSpeed);
+                        }
+                        else
+                        {
+                            GetComponent<Camera>().orthographicSize += (from - current).magnitude;
+                        }
+                        break;
+                    }
             }
         }
-	}
+    }
+
+    
 }
