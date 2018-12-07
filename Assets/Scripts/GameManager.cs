7573acc9309da427f25271e2b834dc1ff8af4c49
Xwilarg <xwilarg@yahoo.fr>
2018-12-07 08:59:30 +0100

    Add main menu

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private bool isMaster;
+
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void LaunchWorker()
+    {
+        isMaster = false;
+        SceneManager.LoadScene("main");
+    }
+
+    public void LaunchMaster()
+    {
+        isMaster = true;
+        SceneManager.LoadScene("main");
+    }
+}
