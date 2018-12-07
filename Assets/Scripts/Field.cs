1b28f1cf44b489c18531e442ff68629a2f8fcfbf
Xwilarg <xwilarg@yahoo.fr>
2018-12-07 10:31:54 +0100

    Add field script

---
+++ Assets/Scripts/Field.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Field : MonoBehaviour
+{
+    private GameManager gm;
+    private DebugPannel debug;
+
+    private void Start()
+    {
+        gm = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+        debug = GameObject.FindGameObjectWithTag("MainCanvas").GetComponent<DebugPannel>();
+    }
+
+    private void OnMouseDown()
+    {
+        gm.CurrentField = this;
+        debug.SetText("Current field: " + name);
+    }
+}
