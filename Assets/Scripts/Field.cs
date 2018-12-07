f5f050303f9a590f502682242102fd8dccd9360f
Xwilarg <xwilarg@yahoo.fr>
2018-12-07 14:51:13 +0100

    Change field mat depending of value

--- Assets/Scripts/Field.cs
+++ Assets/Scripts/Field.cs
@@ -7,10 +7,29 @@ public class Field : MonoBehaviour
     public enum STATE { NONE, OCCUPIED, WIP, DONE, ERROR, SIZE};
     public STATE currentState { get; private set; }
 
+    [SerializeField]
+    private Material CantDo, CanDo, Done;
+
+    private enum Status
+    {
+        CantDo,
+        CanDo,
+        Done
+    }
+
+    [SerializeField]
+    private Db.Work neededWork;
+
     private void Start()
     {
         gm = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
         debug = GameObject.FindGameObjectWithTag("MainCanvas").GetComponent<DebugPannel>();
+        if (gm.work == neededWork)
+            GetComponent<MeshRenderer>().material = CanDo;
+        else if (gm.work < neededWork)
+            GetComponent<MeshRenderer>().material = CantDo;
+        else
+            GetComponent<MeshRenderer>().material = Done;
     }
 
     private void OnMouseDown()
