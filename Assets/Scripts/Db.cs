c8d2da2ad6774c7a73ce8bb58d9d72370c304134
Xwilarg <xwilarg@yahoo.fr>
2018-12-07 09:20:52 +0100

    Add authentification

---
+++ Assets/Scripts/Db.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Db : MonoBehaviour
+{
+    public class Tuple<T, N>
+    {
+        public Tuple(T item1, N item2)
+        {
+            Item1 = item1;
+            Item2 = item2;
+        }
+
+        public T Item1 { private set; get; }
+        public N Item2 { private set; get; }
+    }
+
+    private readonly Dictionary<string, Tuple<string, bool>> users = new Dictionary<string, Tuple<string, bool>>() // Sample Db, need to be refactor using a real one
+    {
+        { "Renia", new Tuple<string, bool>("awak_", true) },
+        { "Jenk", new Tuple<string, bool>("kiRR@", true) },
+        { "Era", new Tuple<string, bool>("rekk", false) },
+        { "Kinoi", new Tuple<string, bool>("_Ino", false) },
+        { "Denko", new Tuple<string, bool>("parr", false) },
+        { "Mino", new Tuple<string, bool>("@w@", false) },
+        { "Wan", new Tuple<string, bool>("dennn", false) }
+    };
+
+    public bool? IsMaster(string user, string password)
+    {
+        if (!users.ContainsKey(user))
+            return (null);
+        var me = users[user];
+        if (me.Item1 == password)
+            return (me.Item2);
+        return (null);
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,24 +1,34 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     private bool isMaster;
+
+    [SerializeField]
+    private Db db;
+    [SerializeField]
+    private InputField userField;
+    [SerializeField]
+    private InputField passwordField;
+    [SerializeField]
+    private Text errorText;
 
     private void Start()
     {
         DontDestroyOnLoad(gameObject);
     }
 
-    public void LaunchWorker()
+    public void Launch()
     {
-        isMaster = false;
-        SceneManager.LoadScene("main");
-    }
-
-    public void LaunchMaster()
-    {
-        isMaster = true;
-        SceneManager.LoadScene("main");
+        bool? res = db.IsMaster(userField.text, passwordField.text);
+        if (res == null)
+            errorText.text = "This user does not exist.";
+        else
+        {
+            isMaster = res.Value;
+            SceneManager.LoadScene("main");
+        }
     }
 }
