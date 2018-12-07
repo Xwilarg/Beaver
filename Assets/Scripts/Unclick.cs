8e755b4da20b421c07ca14f9b60f533e1e91ad60
Xwilarg <xwilarg@yahoo.fr>
2018-12-07 12:46:21 +0100

    Remove field when clicking outside

---
+++ Assets/Scripts/Unclick.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Unclick : MonoBehaviour
+{
+    private GameManager gm;
+    private DebugPannel debug;
+
+    private void Start()
+    {
+        gm = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+        debug = GameObject.FindGameObjectWithTag("MainCanvas").GetComponent<DebugPannel>();
+    }
+
+    private void OnMouseDown()
+    {
+        gm.SetField(null);
+        debug.SetText("");
+    }
+}
